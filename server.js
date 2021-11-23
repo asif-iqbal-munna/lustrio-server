d9d85d8942edda77807c6a1cf89352352e571de9
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-11-23 17:27:41 +0600

    hotels api is created

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const port = process.env.PORT || 8000;
+
+// Middleware
+app.use(cors());
+
+// Database URI with environment variable of username & password
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bauja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+//  Create mongo client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const database = client.db("lustrioData");
+    const hotelsCollection = database.collection("hotels");
+
+    app.get("/hotels", async (req, res) => {
+      const hotels = await await hotelsCollection
+        .find({})
+        .sort({ _id: 1 })
+        .toArray();
+      res.send(hotels);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  console.log("from server");
+  res.send("from server");
+});
+
+app.listen(port, () => {
+  console.log(`listening to the port ${port}`);
+});
